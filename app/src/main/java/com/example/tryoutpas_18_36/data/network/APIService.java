3b6d7accd95a91abb21a6ae30fc9599f8bc449e4
ZacksAryasatya <syafazaqi02@gmail.com>
2025-05-20 02:55:54 +0700

    Add 6 new files for API Spanish Feature

--- app/src/main/java/com/example/tryoutpas_18_36/data/network/APIService.java
+++ app/src/main/java/com/example/tryoutpas_18_36/data/network/APIService.java
@@ -1,5 +1,7 @@
 package com.example.tryoutpas_18_36.data.network;
 
+
+
 import com.example.tryoutpas_18_36.data.model.DataModel;
 
 import retrofit2.Call;
@@ -9,4 +11,7 @@ public interface APIService {
 
     @GET("search_all_teams.php?l=English%20Premier%20League")
     Call<DataModel> getEnglishLeague();
+
+    @GET("search_all_teams.php?s=Soccer&c=Spain")
+    Call<DataModel> getSpanishLeague();
 }
